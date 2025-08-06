24faafcdbc4a1552065707c64479458866ec3432
andjelijaa1 <miticandjelijaa@gmail.com>
2025-08-07 00:32:27 +0200

    almost full version

--- emailing.py
+++ emailing.py
@@ -1,2 +1,38 @@
-def send_email():
-    print("Email was sent")
+import smtplib
+from email.message import EmailMessage
+from PIL import Image
+import io
+
+sender = 'andjelijamdev@gmail.com'
+receiver = 'andjelijamdev@gmail.com'
+password = "nkfk wcum cjuq yomt"
+
+
+def send_email(image_path):
+        emsg = EmailMessage()
+
+        emsg['Subject'] = 'Someone just appeared at the shop!'
+        emsg['From'] = sender
+        emsg['To'] = receiver
+        emsg.set_content('Here is the picture of the person who just entered your shop.')
+
+        with open(image_path, "rb") as file:
+            content = file.read()
+        try:
+            with Image.open(io.BytesIO(content)) as img:
+                subtype = img.format.lower()
+        except IOError:
+            subtype = None
+
+        emsg.add_attachment(content, maintype="image", subtype=subtype)
+
+        gmail = smtplib.SMTP('smtp.gmail.com', 587)
+        gmail.ehlo()
+        gmail.starttls()
+        gmail.login(sender, password)
+        gmail.send_message(emsg)
+        gmail.quit()
+
+
+if __name__ == '__main__':
+    send_email('images/10.png')
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import cv2
 import time
-
+import glob
 from emailing import send_email
+import os
+from threading import Thread
 
 video = cv2.VideoCapture(0)
+
+def clean_images():
+    images = glob.glob('images/*.png')
+    for img in images:
+        os.remove(img)
 
 time.sleep(1)
 first_frame = None
 status_list = []
+count=0
+clean_images()
 while True:
     status = 0
     check, frame = video.read()
@@ -20,7 +29,8 @@ while True:
     delta_frame = cv2.absdiff(first_frame,gray_frame_gau)
 
     thresh_frame = cv2.threshold(delta_frame,63,255,cv2.THRESH_BINARY)[1]
-    dil_frame = cv2.dilate(thresh_frame,None, iterations=2)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    dil_frame = cv2.dilate(thresh_frame, kernel, iterations=2)
     #cv2.imshow("My video", dil_frame)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
@@ -31,12 +41,18 @@ while True:
         rectangle = cv2.rectangle(frame,(x,y),(x+w,h+y),(0,255,0),3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
 
     status_list.append(status)
     status_list = status_list[-2:]
 
-    if status_list[0] == 1 and status_list[1] ==0:
-        send_email()
+    if status_list[0] == 1 and status_list[1] == 0:
+        all_images = glob.glob('images/*.png')
+        index = int(len(all_images) / 2)
+        email_thread = Thread(target=send_email, args=(all_images[index],))
+        email_thread.start()
+
 
     cv2.imshow('Video',frame)
     key = cv2.waitKey(1)
